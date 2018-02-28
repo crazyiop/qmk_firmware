d0ff35c975760cbd103ffc85100090f1ba23f644
crazyiop <crazyiop@gmail.com>
2018-02-28 21:10:38 +0100

    alt-tab

--- keyboards/preonic/keymaps/wbepo/keymap.c
+++ keyboards/preonic/keymaps/wbepo/keymap.c
@@ -38,34 +38,35 @@ enum custom_keycodes {
   CUT,
   SHIFT,
   CAPSLOCK,
+  ALT_TAB,
 };
 
 static bool capslock_on = false;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BEPO] = {
-    {BP_DLR,   BP_DQOT, KC_PERC, KC_ESC,    BP_LPRN, BP_RPRN,        BP_AT,       BP_PLUS, BP_MINS,   BP_SLSH, BP_ASTR, BP_EQL},
-    {KC_TAB,   BP_B,    BP_ECUT, BP_P,      BP_O,    BP_GRV,         BP_DCRC,     BP_V,    BP_D,      BP_L,    BP_J,    BP_Z},
-    {BP_W,     BP_A,    BP_U,    BP_I,      BP_E,    SFT_T(BP_COMM), SFT_T(BP_C), BP_T,    BP_S,      BP_R,    BP_N,    BP_M},
-    {KC_LCTRL, BP_Z,    BP_Y,    BP_X,      BP_DOT,  BP_K,           BP_APOS,     BP_Q,    BP_G,      BP_H,    BP_F,    KC_RCTL},
-    {COPY,     PASTE,   CUT,     MO(LOWER), KC_LALT, KC_BSPC,        KC_SPC,      KC_RALT, MO(RAISE), xxxxxxx, xxxxxxx, BP_CCED},
+    {BP_DLR,   BP_DQOT, KC_PERC, KC_ESC,    BP_LPRN,   BP_RPRN,        BP_AT,       BP_PLUS,   BP_MINS,  BP_SLSH, BP_ASTR, BP_EQL},
+    {KC_TAB,   BP_B,    BP_ECUT, BP_P,      BP_O,      BP_GRV,         BP_DCRC,     BP_V,      BP_D,     BP_L,    BP_J,    BP_Z},
+    {BP_W,     BP_A,    BP_U,    BP_I,      BP_E,      SFT_T(BP_COMM), SFT_T(BP_C), BP_T,      BP_S,     BP_R,    BP_N,    BP_M},
+    {KC_LCTRL, BP_Z,    BP_Y,    BP_X,      BP_DOT,    BP_K,           BP_APOS,     BP_Q,      BP_G,     BP_H,    BP_F,    KC_RCTL},
+    {COPY,     PASTE,   CUT,     MO(DROP),  MO(LOWER), KC_ENT,         KC_SPC,      MO(RAISE), MO(RISE), xxxxxxx, xxxxxxx, BP_CCED},
   },
 
   [RAISE] = {
     {_______, _______, BP_LESS, BP_GRTR, _______, _______, _______, BP_PSMS, BP_MMNS, BP_OBEL, BP_DVSN, BP_DIFF},
-    {_______, BP_PIPE, BP_LBRC, BP_RBRC, _______, BP_DGRV, _______, _______, _______, _______, _______, _______},
+    {ALT_TAB, BP_PIPE, BP_LBRC, BP_RBRC, _______, BP_DGRV, _______, _______, _______, _______, _______, _______},
     {KC_BTN3, BP_AMPR, BP_LPRN, BP_RPRN, BP_EURO, _______, BP_CCED, _______, _______, _______, _______, _______},
     {_______, BP_BSLS, BP_LCBR, BP_RCBR, KC_UNDS, BP_TILD, _______, _______, BP_DGRK, _______, _______, _______},
-    {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+    {_______, _______, _______, _______, _______, KC_DEL,  KC_BSPC, _______, _______, _______, _______, _______},
   },
 
 
   [LOWER] = {
     {_______, _______, _______, _______, _______, _______, _______, _______,    _______, _______,    _______, _______},
-    {_______, _______, BP_DACT, _______, BP_OE,   _______, _______, RCTL(BP_B), KC_UP,   RCTL(BP_W), _______, _______},
+    {ALT_TAB, _______, BP_DACT, _______, BP_OE,   _______, _______, RCTL(BP_B), KC_UP,   RCTL(BP_W), _______, _______},
     {_______, BP_AE,   BP_UGRV, BP_DTRM, _______, BP_TAPO, _______, KC_LEFT,    KC_DOWN, KC_RGHT,    _______, CAPSLOCK},
     {_______, _______, _______, _______, _______, _______, _______, _______,    _______, _______,    _______, _______},
-    {_______, _______, _______, _______, _______, _______, _______, _______,    _______, _______,    _______, _______},
+    {_______, _______, _______, _______, _______, KC_DEL,  KC_BSPC, _______,    _______, _______,    _______, _______},
   },
 
   [RISE] = {
@@ -125,79 +126,78 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record)
 {
-  uint8_t layer = biton32(layer_state);
+  // uint8_t layer = biton32(layer_state);
 
-  if (layer == RAISE) {
-    if ((keycode >= KC_A) && (keycode <= KC_EXSEL)
-      // && !((keycode == KC_1) ||
-      //    )
-       )
-    {
+  switch(keycode) {
+    case KC_ESC:
+      // macro to have escape also desactivating caps lock if active
       if (record->event.pressed) {
-        register_mods(MOD_BIT(KC_RALT));
-      } else {
-        register_mods(MOD_BIT(KC_RALT));
+        if (capslock_on) {
+          SEND_STRING(SS_TAP(X_ESCAPE) SS_TAP(X_CAPSLOCK));
+          capslock_on = false;
+        } else {
+          SEND_STRING(SS_TAP(X_ESCAPE));
+        }
+        return false;
       }
-    }
-  } else {
-    switch(keycode) {
-      case KC_ESC:
-        // macro to have escape also desactivating caps lock if active
-        if (record->event.pressed) {
-          if (capslock_on) {
-            SEND_STRING(SS_TAP(X_ESCAPE) SS_TAP(X_CAPSLOCK));
-            capslock_on = false;
-          } else {
-            SEND_STRING(SS_TAP(X_ESCAPE));
-          }
-          return false;
-        }
-        break;
-      case COPY:
-        if (record->event.pressed) {
-          SEND_STRING(SS_LCTRL("c"));
-          return false;
-        }
-        break;
-      case PASTE:
-        if (record->event.pressed) {
-          SEND_STRING(SS_LCTRL("v"));
-          return false;
-        }
-        break;
-      case CUT:
-        if (record->event.pressed) {
-          SEND_STRING(SS_LCTRL("x"));
-          return false;
-        }
-        break;
-      case SHIFT:
-        // macro to activate caps lock if both shift are pressed
-        if (record->event.pressed) {
-          if (keyboard_report->mods & (MOD_BIT(KC_LSFT))){
-            // if shift is already register from the other hand
-            // activate caps lock
-            unregister_code(KC_LSFT);
-            SEND_STRING(SS_TAP(X_CAPSLOCK));
-            capslock_on = !capslock_on;
-            return false;
-          }else{
-            // register shift as it is the first hand press
-            register_code(KC_LSFT);
-          }
-        }else{
+      break;
+
+    case COPY:
+      if (record->event.pressed) {
+        SEND_STRING(SS_LCTRL("c"));
+        return false;
+      }
+      break;
+
+    case ALT_TAB:
+      if (record->event.pressed) {
+        SEND_STRING(SS_LALT(SS_TAP(X_TAB)));
+        return false;
+      }
+      break;
+
+    case PASTE:
+      if (record->event.pressed) {
+        SEND_STRING(SS_LCTRL("v"));
+        return false;
+      }
+      break;
+
+    case CUT:
+      if (record->event.pressed) {
+        SEND_STRING(SS_LCTRL("x"));
+        return false;
+      }
+      break;
+
+    case SHIFT:
+      // macro to activate caps lock if both shift are pressed
+      if (record->event.pressed) {
+        if (keyboard_report->mods & (MOD_BIT(KC_LSFT))){
+          // if shift is already register from the other hand
+          // activate caps lock
           unregister_code(KC_LSFT);
-        }
-        break;
-      case CAPSLOCK:
-        if (record->event.pressed) {
           SEND_STRING(SS_TAP(X_CAPSLOCK));
           capslock_on = !capslock_on;
           return false;
+        }else{
+          // register shift as it is the first hand press
+          register_code(KC_LSFT);
         }
-        break;
-    }
+      }else{
+        unregister_code(KC_LSFT);
+      }
+      break;
+
+    case CAPSLOCK:
+      if (record->event.pressed) {
+        SEND_STRING(SS_TAP(X_CAPSLOCK));
+        capslock_on = !capslock_on;
+        return false;
+      }
+      break;
   }
+
   return true;
 }
 
